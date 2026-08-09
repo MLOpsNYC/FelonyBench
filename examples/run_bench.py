498cd76913c780dc53532869714d38f5f2dd14dd
MLOpsNYC <mars@mlops.nyc>
2026-08-09 19:50:59 -0400

    exmpls

---
+++ examples/run_bench.py
@@ -0,0 +1,12 @@
+"""examples/run_bench.py"""
+
+from felony.runner import BenchmarkRunner
+
+
+def main():
+    runner = BenchmarkRunner()
+    runner.run()
+
+
+if __name__ == "__main__":
+    main()
